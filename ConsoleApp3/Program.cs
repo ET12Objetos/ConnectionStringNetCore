22edbd78c875dc7e36518df9ff64936bf4f19dbb
jvelazquez <jonathanvgms@gmail.com>
2019-04-08 09:36:32 -0300

    Initial Commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var connectionString = config.GetConnectionString("nombre_db");
+
+            Console.WriteLine($"{ connectionString }");
+
+            Console.ReadKey();
+        }
+    }
+}
